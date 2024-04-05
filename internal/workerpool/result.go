970ef1ccc980a55a8febe47e214e3ce658e7e362
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-04-05 23:04:02 +0300

    feat: impl result struct & its funcs

---
+++ internal/workerpool/result.go
@@ -0,0 +1,21 @@
+package workerpool
+
+import (
+	"fmt"
+	"time"
+)
+
+type Result struct {
+	URL          string
+	StatusCode   int
+	ResponseTime time.Duration
+	Error        error
+}
+
+func (r Result) Info() string {
+	if r.Error != nil {
+		return fmt.Sprintf("[ERROR] - [%s] - %s", r.URL, r.Error.Error())
+	}
+
+	return fmt.Sprintf("[SUCCESS] - [%s] - Status: %d, Response Time: %s", r.URL, r.StatusCode, r.ResponseTime.String())
+}
